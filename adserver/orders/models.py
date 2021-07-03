555cdf39eab4c16e6ca4745582a2f3413eeb6ac7
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 19:20:39 +0200

    AS-30, AS-31, AS-36 created order model and order serializer

--- adserver/orders/models.py
+++ adserver/orders/models.py
@@ -1,3 +1,20 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+
+    class OrderStatus(models.TextChoices):
+        CREATED = "created", "Utworzono"
+        PAID = "paid", "Zapłacono"
+        FAILED = "failed", "Wystapil blad"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
+    name = models.CharField(max_length=64)
+    status = models.CharField(max_length=7, choices=OrderStatus.choices, default=OrderStatus.CREATED)
+    created_at = models.DateTimeField(auto_now=True, auto_created=True)
+    stripe_id = models.CharField(max_length=255, null=True)
+
