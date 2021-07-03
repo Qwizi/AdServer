090fed6ecd2e72177fc60c00bdd09c1dc261b43b
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 17:51:56 +0200

    AS-7 added adupdate serializer

--- adserver/account/views.py
+++ adserver/account/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 
 from ads.models import Ad
-from ads.serializers import AdSerializer
+from ads.serializers import AdSerializer, AdUpdateSerializer
 
 
 class AccountAdViewSet(viewsets.ModelViewSet):
@@ -13,8 +13,12 @@ class AccountAdViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Ad.objects.filter(user=self.request.user)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def get_serializer_class(self, *args, **kwargs):
+        serializer_class = self.serializer_class
 
-    def perform_update(self, serializer):
+        if self.request.method == 'PUT':
+            serializer_class = AdUpdateSerializer
+        return serializer_class
+
+    def perform_create(self, serializer):
         serializer.save(user=self.request.user)
