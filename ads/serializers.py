429d7274aadb1c5f2d6f8d85b44545e59f140ff2
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 14:34:22 +0200

    AS-7 created AccountAdViewSet

--- ads/serializers.py
+++ ads/serializers.py
@@ -6,4 +6,5 @@ from .models import Ad
 class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
-        fields = '__all__'
+        fields = ['id', 'user', 'name', 'url', 'banner', 'balance', 'is_active']
+        read_only_fields = ['user', 'balance', 'is_active']
