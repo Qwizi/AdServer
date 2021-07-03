990d3c8a336d9a27e736fd735eb8bca079ca2e4f
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 13:24:31 +0200

    AS-8 created account serializer and added jwt tokens

--- adserver/account/serializers.py
+++ adserver/account/serializers.py
@@ -1,8 +1,7 @@
-
+from django.conf import settings
 from rest_framework import serializers
-
-from adserver.account.models import Wallet
-
+from django.contrib.auth.models import User
+from .models import Wallet
 
 
 class WalletSerializer(serializers.ModelSerializer):
@@ -10,3 +9,10 @@ class WalletSerializer(serializers.ModelSerializer):
         model = Wallet
         fields = '__all__'
 
+
+class AccountSerializer(serializers.ModelSerializer):
+    wallet = WalletSerializer()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'wallet', 'is_active', 'is_staff']
