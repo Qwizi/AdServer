e7fca27f878a3aa12a749ed503c5c978096d61e0
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 19:38:10 +0200

    AS-32 created account order viewset

--- orders/models.py
+++ orders/models.py
@@ -13,8 +13,9 @@ class Order(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, default="Doładowanie portfela")
     status = models.CharField(max_length=7, choices=OrderStatus.choices, default=OrderStatus.CREATED)
+    amount = models.IntegerField(default=200)
     created_at = models.DateTimeField(auto_now=True, auto_created=True)
     stripe_id = models.CharField(max_length=255, null=True)
 
