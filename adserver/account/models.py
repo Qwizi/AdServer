1064b126da6e7b165a894db2bab41252ead3fdc7
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 12:42:02 +0200

    AS-4 created wallet model

--- adserver/account/models.py
+++ adserver/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Wallet(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wallet")
+    balance = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.username} | {self.balance}"
+
+    def charge(self, value):
+        self.balance += value
+
+    def debit(self, value):
+        self.balance -= value
