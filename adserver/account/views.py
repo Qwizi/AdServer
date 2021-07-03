4d986c46699372bd90511a58343614a6abf38fb1
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 14:34:22 +0200

    AS-7 created AccountAdViewSet

--- adserver/account/views.py
+++ adserver/account/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from ads.models import Ad
+from ads.serializers import AdSerializer
+
+
+class AccountAdViewSet(viewsets.ModelViewSet):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Ad.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
