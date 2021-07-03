7420dfae72e9eb3b4a895dcbe052659341888893
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 18:29:39 +0200

    AS-24 created action charge ad

--- adserver/account/views.py
+++ adserver/account/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
+from account.exceptions import WalletNoFunds
 from ads.models import Ad
-from ads.serializers import AdSerializer, AdUpdateSerializer
+from ads.serializers import AdSerializer, AdUpdateSerializer, AdChargeSerializer
 
 
 class AccountAdViewSet(viewsets.ModelViewSet):
@@ -22,3 +26,23 @@ class AccountAdViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['PUT'])
+    def charge(self, request, pk=None):
+        serializer = AdChargeSerializer(data=request.data)
+        if serializer.is_valid():
+            ad = self.get_object()
+            wallet = request.user.wallet
+            value = serializer.validated_data['value']
+
+            if wallet.balance < value:
+                raise WalletNoFunds()
+
+            wallet.debit(value)
+            ad.charge(value)
+
+            wallet.save()
+            ad.save()
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
