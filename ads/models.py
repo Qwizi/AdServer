ac261c262d2ea12965b414876d80816b3c4bcef0
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 13:40:12 +0200

    AS-12 created ad model

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,16 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Ad(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="ads")
+    url = models.URLField()
+    name = models.CharField(max_length=64)
+    balance = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id} | {self.name} | {self.user.username}"
