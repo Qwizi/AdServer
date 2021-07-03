b04e911ef79c3ff7d0698b06df3e2eb7579fa6bf
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-03 19:06:44 +0200

    AS-26, AS-27 added account ad click/view stats viewsets

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,11 @@ from . import views
 router = routers.SimpleRouter()
 router.register(r'ads', views.AccountAdViewSet)
 
+ads_stats_view_router = routers.NestedSimpleRouter(router, r'ads', lookup='ad')
+ads_stats_view_router.register(r'view-stats', views.AccountAdStatsViewViewSet, basename='ad-view-stats')
+ads_stats_view_router.register(r'click-stats', views.AccountAdStatsClickViewSet, basename='ad-click-stats')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(ads_stats_view_router.urls))
 ]
--- account/views.py
+++ account/views.py
@@ -1,12 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 
 from account.exceptions import WalletNoFunds
-from ads.models import Ad
-from ads.serializers import AdSerializer, AdUpdateSerializer, AdChargeSerializer
+from ads.models import Ad, AdStatsView, AdStatsClick
+from ads.serializers import AdSerializer, AdUpdateSerializer, AdChargeSerializer, AdStatsViewSerializer, \
+    AdStatsClickSerializer
 
 
 class AccountAdViewSet(viewsets.ModelViewSet):
@@ -46,3 +47,21 @@ class AccountAdViewSet(viewsets.ModelViewSet):
 
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AccountAdStatsViewViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = AdStatsView.objects.all()
+    serializer_class = AdStatsViewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return AdStatsView.objects.filter(ad__user=self.request.user, ad__id=self.kwargs['ad_pk'])
+
+
+class AccountAdStatsClickViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = AdStatsClick.objects.all()
+    serializer_class = AdStatsClickSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return AdStatsClick.objects.filter(ad__user=self.request.user, ad__id=self.kwargs['ad_pk'])
