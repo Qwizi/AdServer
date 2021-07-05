1538b407cdd6dddb58512f7164bfede46ee92d6b
Adrian Ciołek <qwizi95@protonmail.com>
2021-07-05 19:39:57 +0200

    AS-69_feature

--- payments/serializers.py
+++ payments/serializers.py
@@ -3,4 +3,6 @@ from rest_framework import serializers
 
 class PaymentSerializer(serializers.Serializer):
     order_id = serializers.UUIDField(required=True, format="hex")
+
+class CreatePaymentMethodSerializer(serializers.Serializer):
     payment_method_id = serializers.CharField(max_length=27, required=True)
--- payments/views.py
+++ payments/views.py
@@ -4,28 +4,11 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-import stripe
+from .stripe import stripe, create_customer, get_customer
 
 from orders.exceptions import InvalidOrder
-from orders.models import Order
+from orders.models import Order, OrderStatus
 from payments.serializers import PaymentSerializer
-
-stripe.api_key = settings.STRIPE_API_KEY
-
-
-def create_customer(email, payment_method_id):
-    # checking if customer with provided email already exists
-    customer_data = stripe.Customer.list(email=email).data
-
-    # if the array is empty it means the email has not been used yet
-    if len(customer_data) == 0:
-        # creating customer
-        customer = stripe.Customer.create(
-            email=email, payment_method=payment_method_id)
-    else:
-        customer = customer_data[0]
-
-    return customer
 
 
 class PaymentsViewSet(viewsets.ViewSet):
@@ -33,32 +16,37 @@ class PaymentsViewSet(viewsets.ViewSet):
 
     def create(self, request):
         serializer = PaymentSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
 
-        user = request.user
-        order_id = serializer.validated_data['order_id']
-        payment_method_id = serializer.validated_data['payment_method_id']
+        if serializer.is_valid():
+            user = request.user
+            order_id = serializer.validated_data['order_id']
 
-        print(order_id)
+            if not Order.objects.filter(id=order_id, user=user).exists():
+                raise InvalidOrder
 
-        if not Order.objects.filter(id=order_id, user=user).exists():
-            raise InvalidOrder
+            order = Order.objects.get(id=order_id, user=user)
 
-        order = Order.objects.get(id=order_id, user=user)
-        customer = create_customer(order.user.email, payment_method_id)
-        payment_intent = stripe.PaymentIntent.create(
-            customer=customer,
-            payment_method=payment_method_id,
-            currency='pln',
-            amount=order.amount,
-            metadata={'order_id': order.id},
-            confirm=True
-        )
-        payment_intent_id = payment_intent['id']
+            if order.status == OrderStatus.PAID:
+                raise InvalidOrder
 
-        order.stripe_id = payment_intent_id
+            customer = get_customer(user.email)
+            payment_method_id = stripe.PaymentMethod.list(customer=customer['id'], type="card").data[0].id
 
-        return Response(data={"status": "success"}, status=status.HTTP_200_OK)
+            payment_intent = stripe.PaymentIntent.create(
+                customer=customer,
+                payment_method=payment_method_id,
+                currency='pln',
+                amount=order.amount,
+                metadata={'order_id': order.id},
+                confirm=True
+            )
+            payment_intent_id = payment_intent['id']
+
+            order.stripe_id = payment_intent_id
+            order.save()
+
+            return Response(data={"status": "success"}, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=["POST"])
     def webhook(self, request):
